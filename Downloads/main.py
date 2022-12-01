8855ad9dd3d1ec4210fa0842958f6cb848329e13
Jeremy Jung <jangho.j@gmail.com>
2022-11-30 20:47:38 -0500

    rgb only

--- Downloads/main.py
+++ Downloads/main.py
@@ -20,11 +20,11 @@ def begin_data_stream():
     tty.setcbreak(sys.stdin.fileno())
     
     rgb_handler = Rgb_Handler(1, 60)
-    thermal_handler = Thermal_Handler(60)
+    # thermal_handler = Thermal_Handler(60)
 
     while True:
         rgb_frame = rgb_handler.get_frame()
-        thermal_frame = thermal_handler.get_frame()
+        # thermal_frame = thermal_handler.get_frame()
 
         cv2.imshow("rgb frame", rgb_frame)
         cv2.imshow("thermal frame", rgb_frame)
@@ -33,11 +33,11 @@ def begin_data_stream():
             cv2.imwrite("rgb{}.jpg".format(rgb_img_num), rgb_frame)
             rgb_img_num += 1
         if (key_is_pressed("b") == True):
-            cv2.imwrite("thermal{}.jpg".format(thermal_img_num), thermal_frame)
+            # cv2.imwrite("thermal{}.jpg".format(thermal_img_num), thermal_frame)
             thermal_img_num += 1
         if (key_is_pressed("q") == True):
             rgb_handler.release()
-            thermal_handler.release()
+            # thermal_handler.release()
             cv2.destroyAllWindows()
 
 begin_data_stream()
