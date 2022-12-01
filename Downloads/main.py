209a605a2b59f6660357e923fa804a39579d4141
Jeremy Jung <jangho.j@gmail.com>
2022-11-30 20:50:40 -0500

    change index to 0

--- Downloads/main.py
+++ Downloads/main.py
@@ -19,28 +19,28 @@ def begin_data_stream():
     thermal_img_num = 0
     tty.setcbreak(sys.stdin.fileno())
     
-    rgb_handler = Rgb_Handler(1, 60)
+    rgb_handler = Rgb_Handler(0, 60)
     # thermal_handler = Thermal_Handler(60)
 
     while True:
-        print("flag AAA")
+        # print("flag AAA")
         rgb_frame = rgb_handler.get_frame()
         # thermal_frame = thermal_handler.get_frame()
-        print("flag A")
+        # print("flag A")
         cv2.imshow("rgb frame", rgb_frame)
         # cv2.imshow("thermal frame", thermal_frame)
-        print("flag B")
+        # print("flag B")
         if (key_is_pressed("a") == True):
             cv2.imwrite("rgb{}.jpg".format(rgb_img_num), rgb_frame)
             rgb_img_num += 1
-        print("flag C")
+        # print("flag C")
         if (key_is_pressed("b") == True):
             # cv2.imwrite("thermal{}.jpg".format(thermal_img_num), thermal_frame)
             thermal_img_num += 1
-        print("flag D")
+        # print("flag D")
         if (key_is_pressed("q") == True):
             rgb_handler.release()
             # thermal_handler.release()
             cv2.destroyAllWindows()
-        print("flag E")
+        # print("flag E")
 begin_data_stream()
