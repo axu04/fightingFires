578d821697285ff81607c53457168787093048e0
Jeremy Jung <jangho.j@gmail.com>
2022-11-30 20:49:11 -0500

    flags

--- Downloads/main.py
+++ Downloads/main.py
@@ -23,21 +23,24 @@ def begin_data_stream():
     # thermal_handler = Thermal_Handler(60)
 
     while True:
+        print("flag AAA")
         rgb_frame = rgb_handler.get_frame()
         # thermal_frame = thermal_handler.get_frame()
-
+        print("flag A")
         cv2.imshow("rgb frame", rgb_frame)
-        cv2.imshow("thermal frame", rgb_frame)
-
+        # cv2.imshow("thermal frame", thermal_frame)
+        print("flag B")
         if (key_is_pressed("a") == True):
             cv2.imwrite("rgb{}.jpg".format(rgb_img_num), rgb_frame)
             rgb_img_num += 1
+        print("flag C")
         if (key_is_pressed("b") == True):
             # cv2.imwrite("thermal{}.jpg".format(thermal_img_num), thermal_frame)
             thermal_img_num += 1
+        print("flag D")
         if (key_is_pressed("q") == True):
             rgb_handler.release()
             # thermal_handler.release()
             cv2.destroyAllWindows()
-
+        print("flag E")
 begin_data_stream()
